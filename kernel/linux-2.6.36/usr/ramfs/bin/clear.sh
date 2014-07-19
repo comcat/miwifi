79dc1b72ca719161684a35bfeca0fcd39c4682d7
Jack Tan <jiankemeng@gmail.com>
2014-07-19 22:30:31 +0800

    kernel: update the clear.sh script in ramfs
    reset the flag in nvram whel load vmlinuz via tftp
    
    Signed-off-by: Jack Tan <jiankemeng@gmail.com>

--- kernel/linux-2.6.36/usr/ramfs/bin/clear.sh
+++ kernel/linux-2.6.36/usr/ramfs/bin/clear.sh
@@ -4,8 +4,11 @@ nvram set flag_tftp_bootup=off
 nvram set flag_package_update=0
 nvram set flag_try_sys1_failed=0
 nvram set flag_try_sys2_failed=0
-nvram set flag_load_sys1_failed=0
-nvram set flag_boot_rootfs=1
+
+nvram set flag_boot_rootfs=0
 nvram set flag_boot_type=1
+nvram unset flag_tftp_booted
+
+nvram unset flag_load_sys1_failed
 nvram commit
 
