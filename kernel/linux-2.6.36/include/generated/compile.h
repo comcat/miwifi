b6be00c2d550f84a73f69a2671bf1393b72abc59
Jack Tan <jiankemeng@gmail.com>
2014-07-22 22:18:37 +0800

    kernel: clear up the temporary file
    Signed-off-by: Jack Tan <jiankemeng@gmail.com>

--- kernel/linux-2.6.36/include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 36 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#36 SMP PREEMPT Fri Jun 20 22:47:20 CST 2014"
-#define LINUX_COMPILE_TIME "22:47:21"
-#define LINUX_COMPILE_BY "comcat"
-#define LINUX_COMPILE_HOST "Pek-JJJ-d1"
-#define LINUX_COMPILE_DOMAIN "(none)"
-#define LINUX_COMPILER "gcc version 4.5.3 (Buildroot 2012.02) "
