5fdb8a313856258645527f2ec7b310b39d34e37d
Jack Tan <jiankemeng@gmail.com>
2014-08-07 17:33:45 +0800

    kernel: update the clear.sh in ramfs
    CFE will load flash.os2 when flag_ota_reboot=1 or
    flag_last_success=1
    
    Signed-off-by: Jack Tan <jiankemeng@gmail.com>

--- kernel/linux-2.6.36/usr/ramfs/bin/clear.sh
+++ kernel/linux-2.6.36/usr/ramfs/bin/clear.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 nvram set flag_tftp_bootup=off
-nvram set flag_package_update=0
-nvram set flag_try_sys1_failed=0
-nvram set flag_try_sys2_failed=0
 
-nvram set flag_boot_rootfs=0
-nvram set flag_boot_type=1
-nvram unset flag_tftp_booted
+nvram unset flag_package_update
+nvram unset flag_try_sys1_failed
+nvram unset flag_try_sys2_failed
+nvram unset flag_boot_rootfs
+nvram unset flag_boot_type
+nvram unset flag_ota_reboot
+nvram unset flag_last_success
 
 nvram unset flag_load_sys1_failed
+nvram unset flag_tftp_booted
+
 nvram commit
 
